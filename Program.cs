ef0cdc8c06f8ef3a8273bc9a6bcc4f9d071027d6
Richard <Richard@DESKTOP-22VVORS>
2024-12-03 12:17:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+//Write a program to print numbers from n to 1 using recursion
+
+//backwards previous assignment
+
+namespace Assignment_5._2_Question_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //input
+            Console.Write("Please enter any number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The numbers backwards from " + n + " to 1 are:");
+            recursion(n);
+        }
+
+        static void recursion(int n)
+            {
+                if (n == 0)
+                {
+                    return;
+                }
+
+                Console.Write(n + " ");
+
+                recursion(n - 1);
+            }
+
+
+    }
+}
+
